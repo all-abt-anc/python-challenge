3c1f884dee51bda4e9573ab61fd79a6e08f77d4a
Afnan Chowdhury <afnanchowdhury@Afnans-MacBook-Pro.local>
2017-10-05 16:32:02 -0400

    modifed PyPoll.py

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -1,9 +1,23 @@
+import os,sys
+import subprocess
+import glob
+from os import path
+import sys
+
+def print_both(file, *args):
+    toprint = ' '.join([arg for arg in args])
+    print toprint
+    file.write(toprint)
+
+file =open('PyPoll.out.txt', 'w')
+
 total_voted = 0
 total_candidate_list = []
 uniq_candidate_list = []
 vote_count_per_candidate_list = []
 avrge_vote_list = []
-with open('/Users/afnanchowdhury/Desktop/GW Bootcamp/Python/python-challenge/PyPoll/raw_data/election_data_2.csv', 'r') as infile:
+
+with open('/Users/afnanchowdhury/Desktop/GW Bootcamp/Python/python-practice/PyPoll/raw_data/election_data_2.csv', 'r') as infile:
     infile.readline()  #skipping the first line, Header
     for row in infile:  #looping between rows
         total_voted = total_voted + 1   #counting total rows or voters
@@ -13,12 +27,11 @@ with open('/Users/afnanchowdhury/Desktop/GW Bootcamp/Python/python-challenge/PyP
         if candidate not in uniq_candidate_list: #applying the condition to get the uniq name
             uniq_candidate_list.append(candidate) #creating new list with uniq names
 
+
 #print(uniq_candidate_list)
-print(
-    '''Election Results
--------------------------''')
-print ('Total Votes:', total_voted)
-print('-------------------------')
+print_both(file,'-------------------------\nElection Results\n-------------------------\nTotal Votes: ' + str(total_voted) + '\n-------------------------\n' )
+
+
 
 for vote_count_per_candidate in uniq_candidate_list: # looping between list of candidates names including duplicate
     vote_count_per_candidate_list.append(total_candidate_list.count(vote_count_per_candidate)) #making a list of how many time candidate names came int he list to get the total vote count for each candidate
@@ -27,14 +40,17 @@ vote_count_per_candidate_dict = dict(zip(uniq_candidate_list,vote_count_per_cand
 for keys in vote_count_per_candidate_dict: #taking the output as mentioned on the example
     avrge_vote = (float(vote_count_per_candidate_dict[keys]))/(float(sum(vote_count_per_candidate_dict.values()))) * 100
     avrge_vote_list.append(avrge_vote)
-    print(keys,': ',str("{0:.2f}".format(avrge_vote)) + '%','('+str(vote_count_per_candidate_dict[keys])+')')
+
+
+    print_both(file, keys + ' : ' + str("{0:.2f}".format(avrge_vote)) + '%' + '('+str(vote_count_per_candidate_dict[keys])+')\n')
+
 
 for keys in vote_count_per_candidate_dict:
     if vote_count_per_candidate_dict[keys] == max(vote_count_per_candidate_dict.values()):
 
-        print('-------------------------''')
-        print ('winner:', keys)
-        print('-------------------------')
+
+        print_both(file,'-------------------------\nwinner: ' + str(keys) + '\n-------------------------')
+
 
 #print(vote_count_per_candidate_dict)
 #print(vote_count_per_candidate_dict[keys])
@@ -43,3 +59,16 @@ for keys in vote_count_per_candidate_dict:
 #print(max(vote_count_per_candidate_dict.values()))
 
 
+#sys.stdout=open('PyPoll.out.txt', 'w')
+#
+#print('Election Results\n-------------------------')
+#print ('Total Votes: ' + str(total_voted))
+#print('-------------------------')
+#print(keys + ' : ' + str("{0:.2f}".format(avrge_vote)) + '%' + '('+str(vote_count_per_candidate_dict[keys])+')')
+#print('-------------------------')
+#print ('winner: ' + str(keys))
+#print('-------------------------')
+#
+#sys.stdout.close()
+
+file.close()
